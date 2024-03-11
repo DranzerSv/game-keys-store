c03e829245b49a16fd48fed34c9f781a970a9265
robertomorales21 <robertoamorales10@gmail.com>
2024-03-11 12:58:19 -0600

    add router to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { PopularGames } from './pages/PopularGames';
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/popular-games" element={<PopularGames />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
