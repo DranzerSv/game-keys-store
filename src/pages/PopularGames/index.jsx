f82e8d57d3201be1641a310db93ca85bf81134a9
Douglas Navarrete <dranzersv@gmail.com>
2024-03-12 18:55:25 -0600

    add api request to popular games

--- src/pages/PopularGames/index.jsx
+++ src/pages/PopularGames/index.jsx
@@ -1,3 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import { getGames } from '../../requests/popular_games_request';
+import { GameCard } from '../../components/GameCard';
+
 export const PopularGames = () => {
-  return <>juegos populares</>;
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['games'],
+    queryFn: () => getGames('mario'),
+  });
+  if (isLoading) return <div>Cargandoooooooo</div>;
+  if (error) return 'An error has occurred:' + error.message;
+  return (
+    <>
+      {data.map((game) => (
+        <GameCard game={game} key={game.id} />
+      ))}
+    </>
+  );
 };
